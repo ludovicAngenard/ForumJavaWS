dae40ae55ece493d1d02d8290446444a223cf715
ludovic <ludovic.angenard@orange.fr>
2021-05-13 14:13:21 +0200

    init project

---
+++ src/main/java/com/ForumJavaWS/demo/ForumJavaWsApplication.java
@@ -0,0 +1,13 @@
+package com.ForumJavaWS.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumJavaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumJavaWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ForumJavaWS/demo/ForumJavaWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForumJavaWS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumJavaWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
