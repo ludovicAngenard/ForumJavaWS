55d8d9279c296f62f96002997061352a50a34cea
ludovic <ludovic.angenard@orange.fr>
2021-05-22 18:57:07 +0200

    Use repository to make sql request

--- src/main/java/com/ForumJavaWS/demo/rest/repository/ReportRepository.java
+++ src/main/java/com/ForumJavaWS/demo/rest/repository/ReportRepository.java
@@ -1,10 +1,16 @@
 package com.ForumJavaWS.demo.rest.repository;
 
+import com.ForumJavaWS.demo.rest.entity.Post;
 import com.ForumJavaWS.demo.rest.entity.Report;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
     public Report findById(Long id);
+
+    public Page<Report> findByPostOrderById(Post post, Pageable pageable);
 }
